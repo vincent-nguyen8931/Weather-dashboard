06e5ef2044d343bc1cfe37b500f569e24e40f4e2
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-01-07 02:54:45 -0800

    Add click function to city names in list to retrieve city's current day and 5 day forecast.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 // function to save a value to specified key
 function setJSON(key, value) {
   window.localStorage.setItem(key, JSON.stringify(value));
@@ -9,30 +10,74 @@ function getJSON(key) {
 
 var weatherBoardMain = $("#weatherBoard");
 // search bar creation variables
-var searchBarTitle = $("<h2>").attr("class", "col-3").text("Search for a City: ");
-var searchBarInputCol = $("<aside>").attr("class", "input-group mb-3");
+var searchBarTitle = $("<h2>").attr("class", "col-3").attr("id", "searchBarTitle").text("Search for a City: ");
+var searchBarInputCol = $("<aside>").attr("class", "input-group mb-3").attr("type", "text");
 var searchBarText = $("<input>").attr("class", "form-control");
-var searchBarButton = $("<button>").attr("class", "icon-search");
+var searchBarButton = $("<button>").attr("class", "icon-search").attr("id", "searchButton");
 
-// countries list creation variables
+// city list creation variable
+var cityListCreation = $("<ul>");
+// city details creation variables
+var cityCurrentDay = $("<h2>").attr("class", "col-8").attr("id", "currentDayBox").text("test");
+var cityCurrentDayTemperature = $("<p>").attr("id", "CurDayTemp");
+var cityCurrentDayHumidity = $("<p>").attr("id", "CurDayHumid");
+var cityCurrentDayWindSpeed = $("<p>").attr("id", "CurDayWind");
+var cityCurrentDayUVIndex = $("<p>").attr("id", "CurDayUV");
 
-//   // makes a new row and increments time by 1 so that another time slot can be created.
-  // $("#weatherBoard").append("<article class='row' style='margin-left:15%;' id=timeRow" + 5 + ">");
-//   $("#timeRow" + i).append("<article class='col-1' style='text-align:right; padding-top:5px; border:1px solid black;'>" + timeText);
-//   $("#timeRow" + i).append("<article class='col' id='plannerText" + i + "'>");
-//   // pulls textarea box from bootstrap and appends it to the column plannerText for each row. used for taking in user input.
-//   $("#plannerText" + i).append("<section class='input-group'>");
-//   $("#plannerText" + i).append("<section class='input-group-append'>");
-//   $("#plannerText" + i).append("<textarea class='form-control' name='textArea" + i + "' id='textArea" + i + "' aria-label='With textarea'>");
-//   // creates a column so that the button image can be appended to it.
-//   $("#timeRow" + i).append("<article class='col-1' style='border:1px solid black; background:cyan;' id=saveIcon" + i + ">");
-//   $("#saveIcon" + i).append("<i class='far fa-save' style='padding-left: 40%; padding-top: 35%;' id=saveBtn>");
-//   // increments by one to move switch statement in changeTIme function down to the next case.
-weatherBoardMain.append(searchBarTitle);
-searchBarTitle.append(searchBarInputCol);
+
+weatherBoardMain.append(searchBarTitle, cityCurrentDay);
+searchBarTitle.append(searchBarInputCol, cityListCreation);
 searchBarInputCol.append(searchBarText, searchBarButton);
+// search button to append input to country list on click.
+$("#searchButton").click(function () {
+  var cityToAdd = $("<li>").text(searchBarText.val());
+  cityListCreation.append(cityToAdd);
 
-// }
+  // var cityName = searchBarText.val();
+
+  // var currentDayWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=e0901fe692dc490e882d515f8c2d9a11";
+
+  // var fiveDayForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=e0901fe692dc490e882d515f8c2d9a11";
+
+  // $.ajax({
+  //   url: currentDayWeatherURL,
+  //   method: "GET"
+  // }).then(function (currentDay) {
+  //   console.log(currentDay);
+  // });
+
+  // $.ajax({
+  //   url: fiveDayForecastURL,
+  //   method: "GET"
+  // }).then(function (fiveDay) {
+
+  // });
+});
+
+// By use of .on for the click event, this allows for the document to continue looking for click events to happen to a li even if one does not presently exist. This method was chosen due to the li being created dynamically and not having static placeholders.
+$("#weatherBoard").on("click", "li", (function () {
+  var cityClicked = this.textContent;
+
+  var currentDayWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityClicked + "&appid=e0901fe692dc490e882d515f8c2d9a11";
+
+  var fiveDayForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityClicked + "&appid=e0901fe692dc490e882d515f8c2d9a11";
+
+  $.ajax({
+    url: currentDayWeatherURL,
+    method: "GET"
+  }).then(function (currentDay) {
+    console.log(currentDay);
+  });
+
+  $.ajax({
+    url: fiveDayForecastURL,
+    method: "GET"
+  }).then(function (fiveDay) {
+    console.log(fiveDay);
+  });
+})
+);
+
 // creates a click event listener on every button in the column that goes into saveInput function.
 // for (i = 1; i < 10; i++) {
 //   $("#saveIcon" + i).click(saveInput);
@@ -44,10 +89,3 @@ searchBarInputCol.append(searchBarText, searchBarButton);
 //   checkRow();
 //   setJSON(rowClicked, $(rowSelected).val());
 // }
-
-// retrieves all information in local storage based on the "events" keys and stores them in their proper row.
-  // for (i = 1; i < 10; i++) {
-  //   rowClicked = "saveIcon" + i;
-  //   checkRow();
-  //   $(rowSelected).val(storageLocation)
-  // }
